866b3bf8212dd81fbba79494b4473e156eb41a6d
Dennis Liu <dennis.liu9@gmail.com>
2022-05-31 03:59:18 +0000

    Added JavaScript to save data entered into form, reset form when submitted

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-
-var $photoURL = document.querySelector('#photo-url');
+var $form = document.querySelector('#entry-area');
 var $photo = document.querySelector('#input-img');
-// var $submitButton = document.querySelector('#save-button');
+var $photoURL = document.querySelector('#photo-url');
 
 function updatePhotoPreview(event) {
-  // test image: https://i.pinimg.com/736x/5c/17/aa/5c17aa712336ec66a892e290f4504a44.jpg (pikachu doing a ✌)
-  if ($photoURL.value !== '') {
-    $photo.setAttribute('src', $photoURL.value);
-    $photo.setAttribute('alt', 'User image');
+  // test image (pikachu doing a ✌): https://i.pinimg.com/736x/5c/17/aa/5c17aa712336ec66a892e290f4504a44.jpg
+  if ($form.elements.photoURL.value !== '') {
+    $photo.setAttribute('src', $form.elements.photoURL.value);
+    $photo.setAttribute('alt', 'User selected image');
   } else {
     $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+}
 
+function saveFormData(event) {
+  event.preventDefault();
+
+  var formData = {
+    entryId: data.nextEntryId,
+    title: $form.elements.entryTitle.value,
+    photoURL: $form.elements.photoURL.value,
+    notes: $form.elements.entryNotes.value
+  };
+  data.entries.unshift(formData);
+  data.nextEntryId++;
+
+  // reset
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $photo.setAttribute('alt', 'placeholder image');
+  $form.reset();
 }
 
 $photoURL.addEventListener('input', updatePhotoPreview);
+$form.addEventListener('submit', saveFormData);
