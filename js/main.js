9383ce754e607cccd861a19fc6cb457bb0f9c112
Dennis Liu <dennis.liu9@gmail.com>
2022-05-31 22:59:27 +0000

    Added code to render entries when switching view to entries, code to switch to entries after submitting new entry

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 /* global data */
 
-// Handle form
+// DOM elements to handle form
 var $form = document.querySelector('#entry-area');
 var $photo = document.querySelector('#input-img');
 var $photoURL = document.querySelector('#photo-url');
 
+// DOM elements to toggle between entry form and entries
+var $views = document.querySelectorAll('div[data-view]');
+var $entriesNav = document.querySelector('#entries-nav');
+var $newEntry = document.querySelector('#new-entry-button');
+
+// Handle form (functions and event listeners)
 function updatePhotoPreview(event) {
   if ($photoURL.value !== '') {
     $photo.setAttribute('src', $photoURL.value);
@@ -17,7 +23,6 @@ function updatePhotoPreview(event) {
 
 function saveFormData(event) {
   event.preventDefault();
-
   var formData = {
     entryId: data.nextEntryId,
     title: $form.elements.entryTitle.value,
@@ -29,15 +34,18 @@ function saveFormData(event) {
   // reset form and image
   $form.reset();
   updatePhotoPreview(null);
+  // show entries
+  hideAllBut($views, 'entries');
 }
 
 $photoURL.addEventListener('input', updatePhotoPreview);
 $form.addEventListener('submit', saveFormData);
 
-// Render journal entries
-var $entriesList = document.querySelector('#entries-list');
+// Render Entries (functions and event listeners)
 
+// Render journal entries according to sample HTML, get data from entryObj
 function renderEntry(entryObj) {
+  // entryObj will be an item in array data.entries
   /* -- Sample Entry HTML --
   <li data-entry-id=[entryObj.entryId]>
     <div class="row">
@@ -87,16 +95,22 @@ function renderEntry(entryObj) {
   return $entryLi;
 }
 
-for (var entryIdx = 0; entryIdx < data.entries.length; entryIdx++) {
-  var $entryTree = renderEntry(data.entries[entryIdx]);
-  $entriesList.appendChild($entryTree);
-}
+/*
+hideAllBut - general function to hide views except desired one
 
-// Toggle between entry form and entries
-var $views = document.querySelectorAll('div[data-view]');
-var $entriesNav = document.querySelector('#entries-nav');
-var $newEntry = document.querySelector('#new-entry-button');
-
+Provided a array of nodes of the data views and a string with the data view to show
+Loop through array
+  Check if the data-view attribute matches the desired one
+    If not, make it hidden by adjusting the class name
+  If it is, set the class name to an empty string
+If dataViewToShow === 'entries', we will want to render the entries tree again
+  Query for the ul element containing all of the entry list items (id entries-list)
+  Clear out all children nodes
+  Loop through data.entries and call renderEntry() on each
+  Append the created DOM tree for each entry to the now cleared entries list
+Update the data object with the now current view
+(This function should be called at the end of the 'submit' event listener)
+*/
 function hideAllBut(views, dataViewToShow) {
   for (var vwIdx = 0; vwIdx < views.length; vwIdx++) {
     if (views[vwIdx].getAttribute('data-view') !== dataViewToShow) {
@@ -105,14 +119,20 @@ function hideAllBut(views, dataViewToShow) {
       views[vwIdx].className = '';
     }
   }
+  if (dataViewToShow === 'entries') {
+    var $entriesList = document.querySelector('#entries-list');
+    $entriesList.replaceChildren();
+    for (var entryIdx = 0; entryIdx < data.entries.length; entryIdx++) {
+      var $entryTree = renderEntry(data.entries[entryIdx]);
+      $entriesList.appendChild($entryTree);
+    }
+  }
+  data.view = dataViewToShow;
 }
 
 $entriesNav.addEventListener('click', function (event) {
   hideAllBut($views, 'entries');
 });
-
 $newEntry.addEventListener('click', function (event) {
   hideAllBut($views, 'entry-form');
 });
-
-// figure out where to put for loop so that it runs when new data is submitted
