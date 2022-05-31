b77124f8dfb8697258bbcf690e661ac9f47eba98
Dennis Liu <dennis.liu9@gmail.com>
2022-05-31 20:21:03 +0000

    Added function that renders a DOM tree given an entry object

--- js/main.js
+++ js/main.js
@@ -32,3 +32,54 @@ function saveFormData(event) {
 
 $photoURL.addEventListener('input', updatePhotoPreview);
 $form.addEventListener('submit', saveFormData);
+
+// render journal entries
+function renderEntry(entryObj) {
+  /* -- Sample Entry HTML --
+  <li data-entry-id=[entryObj.entryId]>
+    <div class="row">
+      <div class="column-half flex-col justify-center">
+        <img src=[entryObj.photoURL] alt="User selected image">
+      </div>
+      <div class="column-half">
+        <h2>[entryObj.title]</h2>
+        <p class="font-regular">
+          [entryObj.notes]
+        </p>
+      </div>
+    </div>
+  </li>
+  */
+
+  var $entryLi = document.createElement('li');
+  $entryLi.setAttribute('data-entry-id', entryObj.entryId);
+
+  var $entryRow = document.createElement('div');
+  $entryRow.className = 'row';
+
+  var $imgCol = document.createElement('div');
+  $imgCol.className = 'column-half flex-col justify-center';
+  var $txtCol = document.createElement('div');
+  $txtCol.className = 'column-half';
+
+  var $entryPhoto = document.createElement('img');
+  $entryPhoto.setAttribute('src', entryObj.photoURL);
+  $entryPhoto.setAttribute('alt', 'User selected image');
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.textContent = entryObj.title;
+  var $entryNotes = document.createElement('p');
+  $entryNotes.textContent = entryObj.notes;
+  $entryNotes.className = 'font-regular';
+
+  $txtCol.appendChild($entryTitle);
+  $txtCol.appendChild($entryNotes);
+  $imgCol.appendChild($entryPhoto);
+
+  $entryRow.appendChild($imgCol);
+  $entryRow.appendChild($txtCol);
+
+  $entryLi.appendChild($entryRow);
+
+  return $entryLi;
+}
