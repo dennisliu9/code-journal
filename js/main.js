b96ed3002da36553a40aaeeebbc6be677073df92
Dennis Liu <dennis.liu9@gmail.com>
2022-06-02 23:00:48 +0000

    Moved globally scoped variable declarations to relevant local scopes where possible. Rearranged code to put function pseudocode inside functions, declarations at the top, execution code at the bottom.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 /* global data */
 
-// Render Entries (functions and event listeners)
-
+// Global Variables
+//    render
 var $entriesList = document.querySelector('#entries-list');
+//    form
+var $form = document.querySelector('#entry-area');
+var $photoURL = document.querySelector('#photo-url');
+//    delete modal
+var $deleteModal = document.querySelector('.modal-screen');
+// event listener targets
+var $entriesNav = document.querySelector('#entries-nav');
+var $newEntry = document.querySelector('#new-entry-button');
+var $deleteButton = document.querySelector('#delete-entry-button');
+var $deleteModalCancel = document.querySelector('#del-modal-cancel');
+var $deleteModalConfirm = document.querySelector('#del-modal-confirm');
 
-// Render journal entries according to sample HTML, get data from entryObj
+// Render Entries
 function renderEntry(entryObj) {
+  // Render journal entries according to sample HTML, get data from entryObj
   // entryObj will be an item in array data.entries
   /* -- Sample Entry HTML --
   <li data-entry-id=[entryObj.entryId]>
@@ -25,7 +37,6 @@ function renderEntry(entryObj) {
     </div>
   </li>
   */
-
   var $entryLi = document.createElement('li');
   $entryLi.setAttribute('data-entry-id', entryObj.entryId);
 
@@ -70,18 +81,18 @@ function renderEntry(entryObj) {
   return $entryLi;
 }
 
-/*
-renderAllEntries - Renders all entries from data.entries as a DOM tree
-This should only return the tree to be appended!
-??? Can I insert things next to? Or not? How should I best build this up
-
-If there are no entries in data.entries
-  Create a p element saying No Entries have been recorded
-Otherwise, if there are entries in data.entries
-  Loop through the entries
-    Call renderEntry() on each
-*/
 function renderAllEntriesOnto(arrayOfChildren, parent) {
+  /*
+  renderAllEntries
+  - calls renderEntry() on each object in arrayOfChildren and appends to parent
+
+  If there are no entries in data.entries
+    Create a p element saying No Entries have been recorded
+  Otherwise, if there are entries in data.entries
+    Loop through the entries
+      Call renderEntry() on each
+      Append result to parent
+  */
   if (arrayOfChildren.length !== 0) {
     for (var entryIdx = 0; entryIdx < arrayOfChildren.length; entryIdx++) {
       var $entryTree = renderEntry(arrayOfChildren[entryIdx]);
@@ -95,21 +106,9 @@ function renderAllEntriesOnto(arrayOfChildren, parent) {
   }
 }
 
-// Render all data for the first time
-renderAllEntriesOnto(data.entries, $entriesList);
-
-// DOM elements to handle form
-var $form = document.querySelector('#entry-area');
-var $photo = document.querySelector('#input-img');
-var $photoURL = document.querySelector('#photo-url');
-
-// DOM elements to toggle between entry form and entries
-// var $views = document.querySelectorAll('.view');
-var $entriesNav = document.querySelector('#entries-nav');
-var $newEntry = document.querySelector('#new-entry-button');
-
-// Handle form (functions and event listeners)
+// Entry Form
 function updatePhotoPreview(event) {
+  var $photo = document.querySelector('#input-img');
   if ($photoURL.value !== '') {
     $photo.setAttribute('src', $photoURL.value);
     $photo.setAttribute('alt', 'User selected image');
@@ -164,37 +163,33 @@ function saveFormData(event) {
     var $elToReplace = $entriesList.querySelector('[data-entry-id="' + data.editing.entryId + '"]');
     $elToReplace.replaceWith(renderEntry(data.entries[entriesUpdateIdx]));
   }
-
   // reset form and image
   resetForm();
   // show entries
   switchToView('entries');
 }
 
-$photoURL.addEventListener('input', updatePhotoPreview);
-$form.addEventListener('submit', saveFormData);
-
-/*
-switchToView - general function to hide views except desired one
-
-Provided a array of nodes of the data views and a string with the data view to show
-Loop through array
-  Check if the data-view attribute matches the desired one
-    If not, make it hidden by adjusting the class name
-  If it is, set the class name to an empty string
-If dataViewToShow === 'entries', we will want to render the entries tree again
-  Query for the ul element containing all of the entry list items (id entries-list)
-  Clear out all children nodes
-  If data.entries is empty
-    Create a new p element object and set its textContent to "No entries have been recorded"
-    Apply styling classes
-    Append as child to the entries list
-  Loop through data.entries and call renderEntry() on each
-  Append the created DOM tree for each entry to the now cleared entries list
-Update the data object with the now current view
-(This function should be called at the end of the 'submit' event listener)
-*/
 function switchToView(dataViewToShow) {
+  /*
+  switchToView - general function to hide views except desired one
+
+  Provided a array of nodes of the data views and a string with the data view to show
+  Loop through array
+    Check if the data-view attribute matches the desired one
+      If not, make it hidden by adjusting the class name
+    If it is, set the class name to an empty string
+  If dataViewToShow === 'entries', we will want to render the entries tree again
+    Query for the ul element containing all of the entry list items (id entries-list)
+    Clear out all children nodes
+    If data.entries is empty
+      Create a new p element object and set its textContent to "No entries have been recorded"
+      Apply styling classes
+      Append as child to the entries list
+    Loop through data.entries and call renderEntry() on each
+    Append the created DOM tree for each entry to the now cleared entries list
+  Update the data object with the now current view
+  (This function should be called at the end of the 'submit' event listener)
+  */
   var $views = document.querySelectorAll('.view');
   for (var vwIdx = 0; vwIdx < $views.length; vwIdx++) {
     if ($views[vwIdx].getAttribute('data-view') !== dataViewToShow) {
@@ -209,34 +204,24 @@ function switchToView(dataViewToShow) {
   data.view = dataViewToShow;
 }
 
-$entriesNav.addEventListener('click', function (event) {
-  switchToView(event.target.getAttribute('data-view'));
-});
-$newEntry.addEventListener('click', function (event) {
-  resetForm();
-  switchToView(event.target.getAttribute('data-view'));
-});
-
 // Edit Button Capabilities
-// var $entriesList = document.querySelector('#entries-list');
-
-/*
-handleEditClick - behavior when an edit icon is clicked
-
-Check if the clicked target is the icon by the tagName
-  If not, return (use a guard)
-Get the parent li element by querying for closest li with data-entry-id
-Get the parent's entry id to search the data object
-  Convert this (and the id in the data object) to a string to standardize types
-Loop through the data.entries array
-  If the current object data.entries has the same entry id as the clicked element
-    Set data.editing to that object
-
-[pre-populate the form]
-Reset the photo preview
-Switch view to entry form
-*/
 function handleEditClick(event) {
+  /*
+  handleEditClick - behavior when an edit icon is clicked
+
+  Check if the clicked target is the icon by the tagName
+    If not, return (use a guard)
+  Get the parent li element by querying for closest li with data-entry-id
+  Get the parent's entry id to search the data object
+    Convert this (and the id in the data object) to a string to standardize types
+  Loop through the data.entries array
+    If the current object data.entries has the same entry id as the clicked element
+      Set data.editing to that object
+
+  [pre-populate the form]
+  Reset the photo preview
+  Switch view to entry form
+  */
   // guard
   if (event.target.tagName !== 'I') {
     return;
@@ -245,36 +230,25 @@ function handleEditClick(event) {
   var $parentLi = event.target.closest('li[data-entry-id]');
   var parentLiEntryId = $parentLi.getAttribute('data-entry-id');
   parentLiEntryId = String(parentLiEntryId);
-
   for (var dataEntriesIdx = 0; dataEntriesIdx < data.entries.length; dataEntriesIdx++) {
     var currentEntryId = String(data.entries[dataEntriesIdx].entryId);
     if (currentEntryId === parentLiEntryId) {
       data.editing = data.entries[dataEntriesIdx];
     }
   }
-
   // pre-populate form
   $form.elements.entryTitle.value = data.editing.title;
   $form.elements.photoURL.value = data.editing.photoURL;
   $form.elements.entryNotes.value = data.editing.notes;
   updatePhotoPreview(null);
-
   // reveal delete button
   $form.elements.deleteEntryButton.classList.remove('invisible');
-
   // find data-view on containing <a> and use to swap
   var $dataViewHolder = event.target.closest('[data-view]');
   switchToView($dataViewHolder.getAttribute('data-view'));
 }
 
-$entriesList.addEventListener('click', handleEditClick);
-
 // Delete Button and Delete Modal
-var $deleteButton = document.querySelector('#delete-entry-button');
-var $deleteModal = document.querySelector('.modal-screen');
-var $deleteModalCancel = document.querySelector('#del-modal-cancel');
-var $deleteModalConfirm = document.querySelector('#del-modal-confirm');
-
 function handleDeleteClick(event) {
   $deleteModal.classList.remove('hidden');
 }
@@ -296,9 +270,28 @@ function handleModalConfirm(event) {
   $deleteModal.classList.add('hidden');
 }
 
+// Finish definitions
+
+// Render all data for the first time
+renderAllEntriesOnto(data.entries, $entriesList);
+
+// Add Event Listeners
+$photoURL.addEventListener('input', updatePhotoPreview);
+$form.addEventListener('submit', saveFormData);
+
+$entriesNav.addEventListener('click', function (event) {
+  switchToView(event.target.getAttribute('data-view'));
+});
+$newEntry.addEventListener('click', function (event) {
+  resetForm();
+  switchToView(event.target.getAttribute('data-view'));
+});
+
+$entriesList.addEventListener('click', handleEditClick);
+
 $deleteButton.addEventListener('click', handleDeleteClick);
 $deleteModalCancel.addEventListener('click', handleModalCancel);
 $deleteModalConfirm.addEventListener('click', handleModalConfirm);
 
-// Show the previous view at the end of the code
+// Show the last view
 switchToView(data.view);
